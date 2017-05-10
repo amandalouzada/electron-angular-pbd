a9958ec3a9d270f9f7768819206b8b0e5e9a72d1
Amanda Louzada <amandanuneslouzada@gmail.com>
2017-05-10 11:26:26 -0300

    Add rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { InserirComponent } from './inserir/inserir.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'inserir',
         component: InserirComponent
+    },
+    {
+        path: '',
+        component: HomeComponent
     }
 ];
 
